e0c37f06571501e53998cb9175e19c1dcec94972
jacqueline-homan <jacquelinehoman7@gmail.com>
2018-01-11 23:11:08 -0500

    first commit

---
+++ conditionals.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+a, b = 0, 1
+if a < b: 
+	print('a({}) is less than b({})' .format(a,b))
+else: 
+	print('a({}) is NOT less than b({})' .format(a,b)) 
+
+print("foo" if a < b else "bar")
