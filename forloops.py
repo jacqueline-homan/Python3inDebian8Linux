f9fcb6454228e4068784f23c371e55453b974c45
jacqueline-homan <jacquelinehoman7@gmail.com>
2018-01-13 01:09:41 -0500

    generator functions

--- forloops.py
+++ forloops.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
 
 # for-loops use an iterator
-
-fh = open('myFirst.txt')
+# Note: You can also use list(fh) to read all the lines of a file
+fh = open('myFirst.txt', 'r+')
+#fh.write('This is a test\n')
 for line in fh:
-	print(line)
+	#print(line)
+	print(line, end='') #this handles the newline
+#list(fh)
