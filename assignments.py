e418ecf617a8ac1627fb2abebd5cd4e292bbfe24
jacqueline-homan <jacquelinehoman7@gmail.com>
2018-01-15 05:19:53 -0500

    illustration of assignments, types and classes

---
+++ assignments.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3 
+
+def main():
+	a = 7
+	b = 'Evan'
+	c = 3.14
+	d = 'a'
+	e = True
+	f = ("WJET", 24)
+
+	print(type(a), a)
+	print(type(b), b)
+	print(type(c), c)
+	print(type(d), d)
+	print(type(e), e)
+	print(type(f), f)
+
+if __name__ == "__main__": main()
